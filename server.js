50ce6f33355295058cdbe9fd1eb5c7b2622b9288
Moe <moealamodi@gmail.com>
2021-01-09 19:33:33 -0500

    added browser.js

--- server.js
+++ server.js
@@ -1,66 +1,73 @@
-
 let express = require('express')
 let mongodb = require('mongodb')
 
 let app = express()
 let db
 
+//app.use(express.static('public'))
+app.use('/public', express.static('/browser.js'));
+
+
 let connectionString = 'mongodb+srv://moealamodi:yA705316@cluster0.eooqa.mongodb.net/ToDoApp?retryWrites=true&w=majority'
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
   db = client.db()
   app.listen(3000)
 })
-
+  
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
-  db.collection('items').find().toArray(function(err, items){
+  db.collection('items').find().toArray(function(err, items) {
     res.send(`<!DOCTYPE html>
-    <html>
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple To-Do App</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    </head>
-    <body>
-    <div class="container">
-    <h1 class="display-4 text-center py-1">To-Do App!</h1>
-    
-    <div class="jumbotron p-3 shadow-sm">
-    <form action="/create-item" method="POST">
-    <div class="d-flex align-items-center">
-    <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-    <button class="btn btn-primary">Add New Item</button>
-    </div>
-    </form>
-    </div>
-    
-    <ul class="list-group pb-5">
-    ${items.map(function(item){
-      return `
-        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+  <html>
+  <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Simple To-Do App</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container">
+  <h1 class="display-4 text-center py-1">To-Do App!</h1>
+  
+  <div class="jumbotron p-3 shadow-sm">
+  <form action="/create-item" method="POST">
+  <div class="d-flex align-items-center">
+  <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+  <button class="btn btn-primary">Add New Item</button>
+  </div>
+  </form>
+  </div>
+  
+  <ul class="list-group pb-5">
+    ${items.map(function(item) {
+      return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
       <span class="item-text">${item.text}</span>
       <div>
       <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
       <button class="delete-me btn btn-danger btn-sm">Delete</button>
       </div>
-      </li> 
-      `
+      </li>`
     }).join('')}
-    
-    </ul>
-    
-    </div>
-    
-    </body>
-    </html>`)
-  })
+  </ul>
   
+  </div>
+  
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+  <script src="/browser.js"></script>
+  </body>
+  </html>`)
+  })
 })
 
 app.post('/create-item', function(req, res) {
   db.collection('items').insertOne({text: req.body.item}, function() {
     res.redirect('/')
   })
+})
+
+app.post('/update-item', function(req, res) {
+  console.log(req.body.text)
+  res.send("Success")
 })
