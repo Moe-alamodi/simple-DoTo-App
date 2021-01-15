c652c54ca1bb1388da53795bce9bdfbbebb13a20
Moe <moealamodi@gmail.com>
2021-01-15 14:13:03 -0500

    added Client-Side Rendering feature

--- server.js
+++ server.js
@@ -1,36 +1,35 @@
 let express = require('express')
 let mongodb = require('mongodb')
-let sanitizeHTML= require('sanitize-html')
+let sanitizeHTML = require('sanitize-html')
 
 let app = express()
 let db
-let port = process.env.PORT
-if (port==null || port == ""){
-  port = 3000
 
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 3000
 }
 
 app.use(express.static('public'))
+
 
 let connectionString = 'mongodb+srv://moealamodi:yA705316@cluster0.eooqa.mongodb.net/ToDoApp?retryWrites=true&w=majority'
 mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
   db = client.db()
   app.listen(port)
 })
-  
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-function passwordProtected(req, res, next){
-  res.set('www-Authenticate','Basic realm= "Simple Todo App"')
+function passwordProtected(req, res, next) {
+  res.set('WWW-Authenticate', 'Basic realm="Simple Todo App"')
   console.log(req.headers.authorization)
   if (req.headers.authorization == "Basic Y2hlbHNlYWZjOmNoZWxzZWE=") {
     next()
   } else {
-    res.status(401).send("Authentication is required")
-
+    res.status(401).send("Authentication required")
   }
-  
 }
 
 app.use(passwordProtected)
@@ -50,45 +49,40 @@ app.get('/', function(req, res) {
   <h1 class="display-4 text-center py-1">To-Do App!</h1>
   
   <div class="jumbotron p-3 shadow-sm">
-  <form action="/create-item" method="POST">
+  <form id="create-form" action="/create-item" method="POST">
   <div class="d-flex align-items-center">
-  <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;" required>
+  <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
   <button class="btn btn-primary">Add New Item</button>
   </div>
   </form>
   </div>
   
-  <ul class="list-group pb-5">
-    ${items.map(function(item) {
-      return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-      <span class="item-text">${item.text}</span>
-      <div>
-      <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-      <button data-id ="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
-      </div>
-      </li>`
-    }).join('')}
+  <ul id="item-list" class="list-group pb-5">
   </ul>
   
   </div>
   
+  <script>
+  let items = ${JSON.stringify(items)}
+  </script>
+
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   <script src="/browser.js"></script>
   </body>
   </html>`)
-  }) 
+  })
 })
 
 app.post('/create-item', function(req, res) {
-  let saveText = sanitizeHTML (req.body.text, {allowedTags: [], allowedAttributes: {}})
-  db.collection('items').insertOne({text: saveText}, function() {
-    res.redirect('/')
+  let safeText = sanitizeHTML(req.body.text, {allowedTags: [], allowedAttributes: {}})
+  db.collection('items').insertOne({text: safeText}, function(err, info) {
+    res.json(info.ops[0])
   })
 })
 
 app.post('/update-item', function(req, res) {
-  let saveText = sanitizeHTML (req.body.text, {allowedTags: [], allowedAttributes: {}})
-  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectID(req.body.id)}, {$set:{text: saveText}}, function(){
+  let safeText = sanitizeHTML(req.body.text, {allowedTags: [], allowedAttributes: {}})
+  db.collection('items').findOneAndUpdate({_id: new mongodb.ObjectId(req.body.id)}, {$set: {text: safeText}}, function() {
     res.send("Success")
   })
 })
